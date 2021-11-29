56618d743b3192dc8d79eb18389612a2ffa33d18
Semen Syrovatskiy <mail@syrovatskiy.tk>
2021-11-29 16:17:54 +0300

    feat: add  descriptor & delete device after close & open device after create

---
+++ src/errors.ts
@@ -0,0 +1,7 @@
+export const createDeviceError = new Error("Cannot create device");
+export const deleteDeviceError = new Error("Cannot delete device");
+export const openDeviceError = new Error("Cannot open device");
+export const closeDeviceError = new Error("Cannot close device");
+export const getDeviceVersionError = new Error("Cannot get device version");
+export const getDeviceMeasurementError = new Error("Cannot get device measurement");
+export const deviceIsClosedError = new Error("Device is closed");
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as ref from "ref-napi";
 
 import libomron from "./libomron";
+import * as errors from "./errors";
 
 export interface OmronMeasurement {
   /** Systolic blood pressure */
@@ -17,11 +18,13 @@ export class OmronDevice {
   private _vid;
   private _pid;
   private _device;
+  private _isOpen;
 
   constructor() {
     this._vid = 0x0590;
     this._pid = 0x0028;
-    this._device = libomron.omron_create();
+    this._device = null;
+    this._isOpen = false;
   }
 
   /** Device vendor ID (default: 0x0590) */
@@ -40,81 +43,106 @@ export class OmronDevice {
     this._pid = pid;
   }
 
+  /** Device open status */
+  get isOpen(): boolean {
+    return this._isOpen;
+  }
+
   /** Open device */
-  open = (): Promise<void> => {
-    const openDeviceError = new Error("Cannot open device");
-    return new Promise<void>((resolve, reject) => {
+  open = async (): Promise<void> => {
+    const createDevice = new Promise<void>((resolve, reject) => {
       try {
-        const ret = libomron.omron_open(this._device, this._vid, this._pid, 0);
-        if (ret < 0) {
-          reject(openDeviceError);
-        } else {
-          resolve();
-        }
+        if (this._device === null) this._device = libomron.omron_create();
+        resolve();
       } catch (err) {
-        reject(openDeviceError);
+        reject(errors.createDeviceError);
       }
     });
+    const openDevice = new Promise<void>((resolve, reject) => {
+      try {
+        const ret = libomron.omron_open(this._device, this._vid, this._pid, 0);
+        if (ret < 0) reject(errors.openDeviceError);
+        this._isOpen = true;
+        resolve();
+      } catch (err) {
+        reject(errors.openDeviceError);
+      }
+    });
+    return createDevice.then(() => openDevice);
   };
 
   /** Close device */
-  close = (): Promise<void> => {
-    const closeDeviceError = new Error("Cannot close device");
-    return new Promise<void>((resolve, reject) => {
+  close = async (): Promise<void> => {
+    const closeDevice = new Promise<void>((resolve, reject) => {
       try {
-        const ret = libomron.omron_close(this._device);
-        if (ret < 0) {
-          reject(closeDeviceError);
-        } else {
-          resolve();
+        if (this._isOpen) {
+          const ret = libomron.omron_close(this._device);
+          if (ret < 0) reject(errors.closeDeviceError);
+          else this._isOpen = false;
         }
+        resolve();
       } catch (err) {
-        reject(closeDeviceError);
+        reject(errors.closeDeviceError);
       }
     });
+    const deleteDevice = new Promise<void>((resolve, reject) => {
+      try {
+        if (this._device !== null) {
+          libomron.omron_delete(this._device);
+          this._device = null;
+        }
+        resolve();
+      } catch (err) {
+        reject(errors.deleteDeviceError);
+      }
+    });
+    return closeDevice.then(() => deleteDevice);
   };
 
   /**
    * Get device version
    * @returns Device version
    */
-  getVersion = (): Promise<string> => {
-    const getVersionError = new Error("Cannot get device version");
-    return new Promise<string>((resolve, reject) => {
+  getVersion = async (): Promise<string> => {
+    const getDeviceVersion = new Promise<string>((resolve, reject) => {
       try {
         const buffer = Buffer.alloc(30 * ref.types.uchar.size).fill(0);
         // @ts-ignore
         const ret = libomron.omron_get_device_version(this._device, buffer);
-        if (ret < 0) {
-          reject(getVersionError);
-        } else {
-          const version = buffer.toString();
-          resolve(version);
-        }
+        if (ret < 0) reject(errors.getDeviceVersionError);
+        const version = buffer.toString();
+        resolve(version);
       } catch (err) {
-        reject(getVersionError);
+        reject(errors.getDeviceVersionError);
+      }
+    });
+    return this._ensureDeviceIsOpen().then(() => getDeviceVersion);
+  };
+
+  private _getMeasurementByIndex = async (index: number): Promise<OmronMeasurement> => {
+    return new Promise<OmronMeasurement>((resolve, reject) => {
+      try {
+        const data = libomron.omron_get_daily_bp_data(this._device, 0, index);
+        if (!data.present) reject(errors.getDeviceMeasurementError);
+        const measurement = {
+          sys: data.sys,
+          dia: data.dia,
+          pul: data.pulse,
+          measuredAt: new Date(2000 + data.year, data.month - 1, data.day, data.hour, data.minute, data.second),
+        } as OmronMeasurement;
+        resolve(measurement);
+      } catch (err) {
+        reject(errors.getDeviceMeasurementError);
       }
     });
   };
 
-  private _getMeasurementByIndex = (index: number): Promise<OmronMeasurement> => {
-    const getMeasurementError = new Error("Cannot get device measurement");
-    return new Promise<OmronMeasurement>((resolve, reject) => {
-      try {
-        const data = libomron.omron_get_daily_bp_data(this._device, 0, index);
-        if (!data.present) {
-          reject(getMeasurementError);
-        } else {
-          const measurement = {
-            sys: data.sys,
-            dia: data.dia,
-            pul: data.pulse,
-            measuredAt: new Date(2000 + data.year, data.month - 1, data.day, data.hour, data.minute, data.second),
-          } as OmronMeasurement;
-          resolve(measurement);
-        }
-      } catch (err) {
-        reject(getMeasurementError);
+  private _ensureDeviceIsOpen = async (): Promise<void> => {
+    return new Promise<void>((resolve, reject) => {
+      if (!this._isOpen) {
+        reject(errors.deviceIsClosedError);
+      } else {
+        resolve();
       }
     });
   };
@@ -124,6 +152,6 @@ export class OmronDevice {
    * @returns Most recent device measurement
    */
   getLastMeasurement = async (): Promise<OmronMeasurement> => {
-    return await this._getMeasurementByIndex(0);
+    return this._ensureDeviceIsOpen().then(() => this._getMeasurementByIndex(0));
   };
 }
