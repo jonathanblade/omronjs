9845851b4b757ddc4a61f76b30811858bce670c6
Semen Syrovatskiy <mail@syrovatskiy.tk>
2021-11-29 13:05:06 +0300

    fix: wrap functions in try catch

--- src/index.ts
+++ src/index.ts
@@ -42,26 +42,34 @@ export class OmronDevice {
 
   /** Open device */
   open = (): Promise<void> => {
+    const openDeviceError = new Error("Cannot open device");
     return new Promise<void>((resolve, reject) => {
-      const ret = libomron.omron_open(this._device, this._vid, this._pid, 0);
-      if (ret < 0) {
-        const err = new Error("Cannot open device");
-        reject(err);
-      } else {
-        resolve();
+      try {
+        const ret = libomron.omron_open(this._device, this._vid, this._pid, 0);
+        if (ret < 0) {
+          reject(openDeviceError);
+        } else {
+          resolve();
+        }
+      } catch (err) {
+        reject(openDeviceError);
       }
     });
   };
 
   /** Close device */
   close = (): Promise<void> => {
+    const closeDeviceError = new Error("Cannot close device");
     return new Promise<void>((resolve, reject) => {
-      const ret = libomron.omron_close(this._device);
-      if (ret < 0) {
-        const err = new Error("Cannot close device");
-        reject(err);
-      } else {
-        resolve();
+      try {
+        const ret = libomron.omron_close(this._device);
+        if (ret < 0) {
+          reject(closeDeviceError);
+        } else {
+          resolve();
+        }
+      } catch (err) {
+        reject(closeDeviceError);
       }
     });
   };
@@ -71,34 +79,42 @@ export class OmronDevice {
    * @returns Device version
    */
   getVersion = (): Promise<string> => {
+    const getVersionError = new Error("Cannot get device version");
     return new Promise<string>((resolve, reject) => {
-      const buffer = Buffer.alloc(30 * ref.types.uchar.size).fill(0);
-      // @ts-ignore
-      const ret = libomron.omron_get_device_version(this._device, buffer);
-      if (ret < 0) {
-        const err = new Error("Cannot get device version");
-        reject(err);
-      } else {
-        const version = buffer.toString();
-        resolve(version);
+      try {
+        const buffer = Buffer.alloc(30 * ref.types.uchar.size).fill(0);
+        // @ts-ignore
+        const ret = libomron.omron_get_device_version(this._device, buffer);
+        if (ret < 0) {
+          reject(getVersionError);
+        } else {
+          const version = buffer.toString();
+          resolve(version);
+        }
+      } catch (err) {
+        reject(getVersionError);
       }
     });
   };
 
   private _getMeasurementByIndex = (index: number): Promise<OmronMeasurement> => {
+    const getMeasurementError = new Error("Cannot get device measurement");
     return new Promise<OmronMeasurement>((resolve, reject) => {
-      const data = libomron.omron_get_daily_bp_data(this._device, 0, index);
-      if (!data.present) {
-        const err = new Error(`Measurement with index=${index} is not present`);
-        reject(err);
-      } else {
-        const measurement = {
-          sys: data.sys,
-          dia: data.dia,
-          pul: data.pulse,
-          measuredAt: new Date(2000 + data.year, data.month - 1, data.day, data.hour, data.minute, data.second),
-        } as OmronMeasurement;
-        resolve(measurement);
+      try {
+        const data = libomron.omron_get_daily_bp_data(this._device, 0, index);
+        if (!data.present) {
+          reject(getMeasurementError);
+        } else {
+          const measurement = {
+            sys: data.sys,
+            dia: data.dia,
+            pul: data.pulse,
+            measuredAt: new Date(2000 + data.year, data.month - 1, data.day, data.hour, data.minute, data.second),
+          } as OmronMeasurement;
+          resolve(measurement);
+        }
+      } catch (err) {
+        reject(getMeasurementError);
       }
     });
   };
